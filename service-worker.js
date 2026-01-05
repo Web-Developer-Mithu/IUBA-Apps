1beede85bfa37f97d041296a240c444a7dbbd8c1
Md. Mithu Rahman <digital.marketer.mithu@gmail.com>
2026-01-05 17:38:39 +0600

    aa

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,4 @@
-const CACHE_NAME = 'iuba-pwa-cache-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
